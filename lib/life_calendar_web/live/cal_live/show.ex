f5591f41c7553c5f4f72543c15a6fa19b4969326
qodot <waitingforqodot@gmail.com>
2023-04-16 10:43:20 +0900

    change CalLive.Show page_title

--- lib/life_calendar_web/live/cal_live/show.ex
+++ lib/life_calendar_web/live/cal_live/show.ex
@@ -10,12 +10,14 @@ defmodule LifeCalendarWeb.CalLive.Show do
 
   @impl true
   def handle_params(%{"id" => id}, _, socket) do
+    cal = Cals.get_cal!(id)
+
     {:noreply,
      socket
-     |> assign(:page_title, page_title(socket.assigns.live_action))
-     |> assign(:cal, Cals.get_cal!(id))}
+     |> assign(:page_title, page_title(socket.assigns.live_action, cal))
+     |> assign(:cal, cal)}
   end
 
-  defp page_title(:show), do: "Show Cal"
-  defp page_title(:edit), do: "Edit Cal"
+  defp page_title(:show, cal), do: cal.name
+  defp page_title(:edit, cal), do: "#{cal.name} 수정"
 end
