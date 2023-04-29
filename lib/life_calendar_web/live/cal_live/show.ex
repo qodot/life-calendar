fe7874854bfa571a6f73718459033ee72d84e5b8
qodot <waitingforqodot@gmail.com>
2023-04-29 23:43:16 +0900

    Add permission check for current user before showing calendar in CalLive.Show

--- lib/life_calendar_web/live/cal_live/show.ex
+++ lib/life_calendar_web/live/cal_live/show.ex
@@ -15,20 +15,31 @@ defmodule LifeCalendarWeb.CalLive.Show do
     try do
       cal = Cals.get_cal!(id)
 
+      if cal.user_id != socket.assigns.current_user.id do
+        raise "NoPermissionError"
+      end
+
       {:noreply,
        socket
        |> assign(:page_title, page_title(socket.assigns.live_action, cal))
        |> assign(:cal, cal)
        |> assign(:years, cal |> Cal.years())}
     rescue
-      exception in Ecto.NoResultsError ->
-        IO.inspect(exception)
+      Ecto.NoResultsError ->
+        socket |> redirect_to_cals()
 
-        {:noreply,
-         socket
-         |> put_flash(:error, "존재하지 않는 캘린더입니다.")
-         |> push_patch(to: "/cals")}
+      e in RuntimeError ->
+        case e.message do
+          "NoPermissionError" -> socket |> redirect_to_cals()
+        end
     end
+  end
+
+  defp redirect_to_cals(socket) do
+    {:noreply,
+     socket
+     |> put_flash(:error, "존재하지 않는 캘린더입니다.")
+     |> push_patch(to: "/cals")}
   end
 
   defp page_title(:show, cal), do: cal.name
