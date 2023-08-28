61b17578d95e754d5a70ae35a9672cbff5865619
qodot <waitingforqodot@gmail.com>
2023-08-28 15:53:53 +0900

    `Cal.years.weeks.time_type` 테스트

--- test/life_calendar/cals/cal_test.exs
+++ test/life_calendar/cals/cal_test.exs
@@ -15,16 +15,28 @@ defmodule LifeCalendar.Cals.CalTest do
       assert first_year.year == 1988
 
       first_week_of_first_year = first_year.weeks |> Enum.at(0)
-      assert first_week_of_first_year.past? == true
+      assert first_week_of_first_year.time_type == :before_born
+
+      last_week_of_first_year = first_year.weeks |> Enum.at(-1)
+      assert last_week_of_first_year.time_type == :past
 
       today_year = years |> Enum.at(Date.utc_today().year - 1988)
       assert today_year.year == Date.utc_today().year
 
       first_week_of_today_year = today_year.weeks |> Enum.at(0)
-      assert first_week_of_first_year.past? == true
+      assert first_week_of_today_year.time_type == :past
 
       last_week_of_today_year = today_year.weeks |> Enum.at(-1)
-      assert last_week_of_today_year.past? == false
+      assert last_week_of_today_year.time_type == :future
+
+      last_year = years |> Enum.at(-1)
+      assert last_year.year == 1988 + lifespan
+
+      first_week_of_last_year = last_year.weeks |> Enum.at(0)
+      assert first_week_of_last_year.time_type == :old
+
+      last_week_of_last_year = last_year.weeks |> Enum.at(-1)
+      assert last_week_of_last_year.time_type == :after_death
     end
   end
 end
